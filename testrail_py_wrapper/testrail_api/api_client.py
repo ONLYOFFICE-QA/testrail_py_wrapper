5cb077af53f740a9a2ceecd81ed1b5c765411579
l02 <l8556@outlook.com>
2025-02-21 17:38:37 +0300

    rework cache

---
+++ testrail_py_wrapper/testrail_api/api_client.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from typing import Optional, Dict, Any
+
+import aiohttp
+
+
+class APIClient:
+    __cache = {}
+
+    def __init__(self, base_url: str, username: str, password: str) -> None:
+        self.base_url = base_url
+        self.aio_http_auth = aiohttp.BasicAuth(username, password)
+
+    async def request(self, method: str, endpoint: str, data: Optional[Dict[str, Any]] = None) -> Dict[str, Any]:
+        cache_key = (method, endpoint, frozenset(data.items()) if data else None)
+
+        if method == "GET" and cache_key in self.__cache:
+            return self.__cache[cache_key]
+
+        async with aiohttp.ClientSession(auth=self.aio_http_auth) as session:
+            url = self.base_url + endpoint
+            async with session.request(method, url, json=data) as response:
+                result = await response.json()
+
+                if method == "GET":
+                    self.__cache[cache_key] = result
+
+                return result
